e73b3e7c5e0701724b320a427d71776f8d313923
benkarr <64742466+Ben-Karr@users.noreply.github.com>
2022-10-05 13:02:41 +0200

    bu state

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /code
 
 COPY ./app /code/app
 
-CMD uvicorn app.main:app --host 0.0.0.0 --port=${PORT}
+CMD uvicorn app.main:app --host 0.0.0.0 --port=$PORT
--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
-from typing import Union
-from fastapi import FastAPI
+#from typing import Union
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 from fastai.vision.all import *
 
 app = FastAPI()
 
-path = Path('./app/models')
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+## path = Path('./app/models') ## docker / heroku
+path = Path('./models') ## local
 file_path = path/"bird_classifier_resnet18_963.pkl"
 
 learn = load_learner(file_path)
@@ -13,10 +29,13 @@ classes = learn.dls.vocab
 
 def predict(img):
     label, _, probs = learn.predict(img)
-    return label, zip(classes, list(map(float, probs)))
-    #return 'robin', {'robin': 0.081, 'banana': 0.019}
+    return {'label': label, 'confidences': zip(classes, list(map(float, probs)))}
 
 @app.get("/")
 def read_root():
-    label, probs = predict('./app/example.jpeg')
-    return {"label": label, "conidences": probs}
+    #label, probs = predict('./app/example.jpeg') ## docker / heroku
+    #label, probs = predict('./example.jpeg') ## local
+
+    results = predict('./examle.jpeg')
+    ## return {"label": label, "confidences": probs}
+    return JSONResponse(content=jsonable_encoder(result))
